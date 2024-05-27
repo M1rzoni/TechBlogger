c93eb039c5732122abd1c49fdde9cd10762174b0
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-05-27 18:57:12 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="green">
+      Navbar
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Navbar;
